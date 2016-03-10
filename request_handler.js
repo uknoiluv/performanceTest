caa253bab756c3da9700d26cc444679c4497e035
Adrian Kim <adrian@Adrians-MacBook-Pro.local>
2016-03-10 12:06:11 -0800

    20160310_create_endpoint_for_array20160310_create_endpoint_for_array

---
+++ request_handler.js
@@ -0,0 +1,4 @@
+
+exports.getArray = function(req, res) {
+  res.status(200).send([1, 2, 3]);  
+};
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var handler = require('./request_handler');
+
+
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
@@ -6,6 +9,8 @@ app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res){
   res.redirect('/index.html');
 });
+
+app.get('/array', handler.getArray);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
