0eeb63516ef8d1e22f839e0d05a49f3075eb2515
Adrian Kim <adrian@Adrians-MacBook-Pro.local>
2016-03-11 09:56:37 -0800

    20160311_add_nested_ng_repeat

--- app/controller.js
+++ app/controller.js
@@ -4,7 +4,16 @@
     .controller('testController', function(myService){
       // do stuff with scope
       var testScope = this;
-      testScope.hello = 'hello world';
+      testScope.data = [];
+      testScope.createNestedArray = function() {
+        for(var i = 0; i < 10; i++) {
+          testScope.data.push([]);
+          for(var j = 0; j < 10; j++) {
+            testScope.data[i].push(10 * i + j)
+          }
+        }
+      };
+
       myService.getArray(function(data) {
       	console.log('data', data);
       	testScope.array = data;
